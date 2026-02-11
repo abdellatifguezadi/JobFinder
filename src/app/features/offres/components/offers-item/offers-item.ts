ac09be228081521b39b350494768d0e5a7c725de
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-02-11 15:26:48 +0100

    feat : implement affichage and desaffichage des boutons de favorite et de postuler dans la page des offres a condition que l'utilisateur est connecté

--- src/app/features/offres/components/offers-item/offers-item.ts
+++ src/app/features/offres/components/offers-item/offers-item.ts
@@ -1,13 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { JobOffer } from '../../../../core/model/job-offer';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as authSelectore from '../../../../core/store/auth/auth.selectors'
 
 @Component({
   selector: 'app-offers-item',
-  imports: [DatePipe],
+  imports: [DatePipe, AsyncPipe],
   templateUrl: './offers-item.html',
   styleUrl: './offers-item.css',
 })
 export class OffersItem {
   job = input.required<JobOffer>();
+  store = inject(Store);
+
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(){
+    this.isAuthenticated$ = this.store.select(authSelectore.selectAuthenticated);
+  }
 }
